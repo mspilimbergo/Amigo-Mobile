51833ff2f6e64a9130d429eed100202b43b15ca3
dylans123 <dylanskelly@knights.ucf.edu>
2020-03-22 01:25:16 -0400

    Added basic routing from login to register page

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import './register_screen.dart';
 import '../main/main_screen.dart';
 
 final storage = FlutterSecureStorage();
@@ -29,17 +30,6 @@ class LoginPage extends StatelessWidget {
     );
     if(res.statusCode == 200) return res.body;
     return null;
-  }
-
-  Future<int> attemptSignUp(String email, String password) async {
-    var res = await http.post(
-      '$SERVER_URL/api/signup',
-      body: {
-        "email": email,
-        "password": password
-      }
-    );
-    return res.statusCode;
   }
 
   @override
@@ -85,23 +75,12 @@ class LoginPage extends StatelessWidget {
             ),
             FlatButton(
               onPressed: () async {
-                var email = _emailController.text;
-                var password = _passwordController.text;
-
-                if(email.length < 4) 
-                  displayDialog(context, "Invalid email", "The email should be at least 4 characters long");
-                else if(password.length < 4) 
-                  displayDialog(context, "Invalid Password", "The password should be at least 4 characters long");
-                else{
-                  var res = await attemptSignUp(email, password);
-                  if(res == 201)
-                    displayDialog(context, "Success", "The user was created. Log in now.");
-                  else if(res == 409)
-                    displayDialog(context, "That email is already registered", "Please try to sign up using another email or log in if you already have an account.");  
-                  else {
-                    displayDialog(context, "Error", "An unknown error occurred.");
-                  }
-                }
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RegisterPage(),
+                  ),
+                );
               },
               child: Text("Sign Up")
             )
--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+import './login_screen.dart';
+import '../main/main_screen.dart';
 
-class RegisterScreen extends StatelessWidget {
+final storage = FlutterSecureStorage();
+final SERVER_URL = "http://10.0.2.2:3000";
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void displayDialog(context, title, text) => showDialog(
+    context: context,
+    builder: (context) =>
+      AlertDialog(
+        title: Text(title),
+        content: Text(text)
+      ),
+  );
+
+  Future<int> attemptSignUp(String email, String password) async {
+    var res = await http.post(
+      '$SERVER_URL/api/signup',
+      body: {
+        "email": email,
+        "password": password
+      }
+    );
+    return res.statusCode;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Text(
-      'Hello'
+    return Scaffold(
+      appBar: AppBar(title: Text("Sign Up"),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email'
+              ),
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password'
+              ),
+            ),
+            FlatButton(
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage()
+                  ),
+                );
+              },
+              child: Text("Log In")
+            ),
+            FlatButton(
+              onPressed: () async {
+                var email = _emailController.text;
+                var password = _passwordController.text;
+
+                if(email.length < 4) 
+                  displayDialog(context, "Invalid email", "The email should be at least 4 characters long");
+                else if(password.length < 4) 
+                  displayDialog(context, "Invalid Password", "The password should be at least 4 characters long");
+                else{
+                  var res = await attemptSignUp(email, password);
+                  if(res == 201)
+                    displayDialog(context, "Success", "The user was created. Log in now.");
+                  else if(res == 409)
+                    displayDialog(context, "That email is already registered", "Please try to sign up using another email or log in if you already have an account.");  
+                  else {
+                    displayDialog(context, "Error", "An unknown error occurred.");
+                  }
+                }
+              },
+              child: Text("Sign Up")
+            )
+          ],
+        ),
+      )
     );
   }
 }
