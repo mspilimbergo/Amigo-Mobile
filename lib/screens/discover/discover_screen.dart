0427f6217f64c6979f8835a325139356e9731fc4
Jerold Lodge <jrld1999@gmail.com>
2020-03-23 14:48:07 -0400

    Custom tagCard widget

--- lib/screens/discover/discover_screen.dart
+++ lib/screens/discover/discover_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-class DiscoverPage extends StatelessWidget {
+class TagCard extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: 200,
-        child: Card(
-          elevation: 5,
-          margin: EdgeInsets.all(10),
-        ),
+    return Container(
+      child: Card(
+        child: Text("My Card"),
       )
+    );
+  }
+}
+
+class DiscoverPage extends StatefulWidget {
+  @override
+  _DiscoverPageState createState() => _DiscoverPageState();
+}
+
+class _DiscoverPageState extends State<DiscoverPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TagCard(),
+      color: Colors.red,
     );
   }
 }
