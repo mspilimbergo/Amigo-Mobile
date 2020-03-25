8484d6cb22850a6be4fedb5cc01d08adf8dae557
Jerold <jrld1999@gmail.com>
2020-03-25 16:02:43 -0400

    3 column grid, transparent app bar

--- lib/screens/discover/discover_tag_view.dart
+++ lib/screens/discover/discover_tag_view.dart
@@ -1,31 +1,8 @@
 import 'package:flutter/material.dart';
-import '../discover/discover_channel_view/discover_channel_view.dart';
+import './widgets/TagButton.dart';
 
-String img =
-    "https://media-exp1.licdn.com/dms/image/C560BAQG4QXbbg39AfQ/company-logo_100_100/0?e=2159024400&v=beta&t=QYCFMlTBClczprYLrvWL1W4sbCrWw0TmGfuUTapBmDY";
-
-class TagButton extends StatelessWidget {
-  final int tagID;
-  final String name;
-
-  TagButton(this.tagID, this.name);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Column(children: <Widget>[
-      Container(
-        height: 125,
-        width: 125,
-        decoration: BoxDecoration(
-          image: DecorationImage(image: NetworkImage(img), fit: BoxFit.cover),
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      Text("Basketball", style: TextStyle(fontSize: 16)),
-    ]));
-  }
-}
+//String img = "https://media-exp1.licdn.com/dms/image/C560BAQG4QXbbg39AfQ/company-logo_100_100/0?e=2159024400&v=beta&t=QYCFMlTBClczprYLrvWL1W4sbCrWw0TmGfuUTapBmDY";
+String randimg = "https://source.unsplash.com/random";
 
 class DiscoverTagView extends StatefulWidget {
   @override
@@ -36,15 +13,27 @@ class _DiscoverTagViewState extends State<DiscoverTagView> {
   String searchTag;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-                margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
                 child: Text("What are your interests?",
-                    style: TextStyle(color: Colors.red, fontSize: 22))),
+                    style: TextStyle(color: Colors.red, fontSize: 22)),
+              )
+            ],
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+                  Widget>[
             Container(
                 height: 40,
                 margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
@@ -56,28 +45,29 @@ class _DiscoverTagViewState extends State<DiscoverTagView> {
                   ),
                 )),
             Container(
-              margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+              margin: EdgeInsets.fromLTRB(20, 1, 0, 10),
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
               child: RichText(
                   text: TextSpan(
                       style: Theme.of(context).textTheme.body1,
                       children: [
                     WidgetSpan(child: Icon(Icons.pin_drop, size: 20)),
-                    TextSpan(text: "Orlando"),
+                    TextSpan(text: "University of Central Florida"),
                   ])),
             ),
-            Container(
-                child: Center(
-                    child: RaisedButton(
-              child: Text('Navigate to Channel View'),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => DiscoverChannelView(
-                              tagSelected: 'Sports',
-                            )));
-              },
-            ))),
+            // Container(
+            //     child: Center(
+            //         child: RaisedButton(
+            //   child: Text('Navigate to Channel View'),
+            //   onPressed: () {
+            //   Navigator.push(
+            //       context,
+            //       MaterialPageRoute(
+            //           builder: (context) => DiscoverChannelView(
+            //                 tagSelected: 'Sports',
+            //               )));
+            // },
+            // ))),
             Expanded(
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -98,8 +88,9 @@ class _DiscoverTagViewState extends State<DiscoverTagView> {
                               scrollDirection: Axis.horizontal,
                               itemBuilder: (context, index) {
                                 return Container(
-                                  margin: EdgeInsets.symmetric(horizontal: 5),
-                                  child: TagButton(index, "Basketball"),
+                                  margin: EdgeInsets.symmetric(horizontal: 10),
+                                  child:
+                                      TagButton("$index", "Basketball", randimg),
                                 );
                               })),
                     ),
@@ -112,16 +103,16 @@ class _DiscoverTagViewState extends State<DiscoverTagView> {
                                     fontSize: 18)))),
                     SliverGrid(
                         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
+                          crossAxisCount: 3,
                           mainAxisSpacing: 20,
                           crossAxisSpacing: 0,
-                          childAspectRatio: 1.4,
+                          childAspectRatio: .95,
                         ),
                         delegate: SliverChildBuilderDelegate(
                           (BuildContext context, int index) {
                             return Container(
                               margin: EdgeInsets.symmetric(horizontal: 5),
-                              child: TagButton(index, "Basketball"),
+                              child: TagButton("$index", "Basketball", randimg),
                             );
                           },
                           childCount: 20,
@@ -133,6 +124,6 @@ class _DiscoverTagViewState extends State<DiscoverTagView> {
               children: <Widget>[],
             ))
           ]),
-    );
+        ));
   }
 }
