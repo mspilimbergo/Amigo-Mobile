3c2bf12166431d7399e4c8b133bd584bf4daa6fc
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-04 02:46:34 -0400

    Added fix to prevent back arrow from appearing

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -77,220 +77,223 @@ class _HomePageState extends State<HomePage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NestedScrollView(
-        controller: _scrollController,
-        headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              title: Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: Text(
-                    'Chats',
-                    style: TextStyle(fontSize: 30.0, color: Colors.red[200])
-                  ),
-              ),
-              pinned: true,
-              floating: true,
-              forceElevated: boxIsScrolled,
-              backgroundColor: Colors.white,
-              actions: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(right: 20.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      showSearch(
-                        context: context,
-                        delegate: UserSearchDelegate(display: displayName, sender: userId)
-                      );
-                    },
-                    child: Icon(
-                      Icons.add_box,
-                      size: 26.0,
-                      color: Colors.red[200],
+    return new WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: NestedScrollView(
+          controller: _scrollController,
+          headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                      'Chats',
+                      style: TextStyle(fontSize: 30.0, color: Colors.red[200])
                     ),
+                ),
+                pinned: true,
+                floating: true,
+                forceElevated: boxIsScrolled,
+                backgroundColor: Colors.white,
+                actions: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        showSearch(
+                          context: context,
+                          delegate: UserSearchDelegate(display: displayName, sender: userId)
+                        );
+                      },
+                      child: Icon(
+                        Icons.add_box,
+                        size: 26.0,
+                        color: Colors.red[200],
+                      ),
+                    )
                   )
+                ],
+                bottom: new TabBar(
+                  controller: _tabController,
+                  indicatorColor: Colors.red[200],
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  tabs: <Tab>[
+                    new Tab(text: "Groups"),
+                    new Tab(text: "Direct"),
+                  ]
                 )
-              ],
-              bottom: new TabBar(
-                controller: _tabController,
-                indicatorColor: Colors.red[200],
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.grey,
-                tabs: <Tab>[
-                  new Tab(text: "Groups"),
-                  new Tab(text: "Direct"),
-                ]
               )
+            ];
+          },
+          body: TabBarView(
+            controller: _tabController,
+            children: <Widget> [
+              FutureBuilder<String>(
+                future: getChannels(),
+                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  List<Widget> children;
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData && snapshot.data != null) {
+                      var jsonSnap = json.decode(snapshot.data);
+                      if (jsonSnap["channels"].length == 0) {
+                        children = <Widget>[
+                          new Container(
+                            color: Colors.white,
+                            child: Center(
+                              child: Text(
+                                "You haven't joined any channels yet! Head over to the discover page to join some!"
+                              )
+                            )
+                          )
+                        ];
+                      } else {
+                        children = <Widget>[
+                          ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: jsonSnap["channels"].length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage: NetworkImage("https://picsum.photos/seed/picsum/200"),
+                                ),
+                                title: Text(jsonSnap["channels"][index]["name"]),
+                                subtitle: Text(jsonSnap["channels"][index]["description"]),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ChatPage(name: jsonSnap["channels"][index]["name"], id: jsonSnap["channels"][index]["channel_id"], display: displayName, sender: userId, direct: false)
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        ];
+                      }
+                    } else if (snapshot.hasError) {
+                      children = <Widget>[
+                        new Container(
+                          color: Colors.white,
+                          child: Center(
+                            child: Text(
+                              "There was an error fetching your channels, please check your internet connection and try again in a second."
+                            )
+                          )
+                        )
+                      ];
+                    }
+                  } else {
+                    return new Container(
+                      color: Colors.white,
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.white,
+                          valueColor: new AlwaysStoppedAnimation<Color>(Colors.red[200])
+                        )
+                      )
+                    );
+                  }
+                  return Container(
+                    color: Colors.white,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: children,
+                      )
+                    ),
+                  );
+                }
+              ),
+              FutureBuilder<String>(
+                future: getDirect(),
+                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  List<Widget> children;
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData && snapshot.data != null) {
+                      var jsonSnap = json.decode(snapshot.data);
+                      if (jsonSnap["users"].length == 0) {
+                        children = <Widget>[
+                          new Container(
+                            color: Colors.white,
+                            child: Center(
+                              child: Text(
+                                "You haven't sent any direct messages yet! Use the button above to send some!"
+                              )
+                            )
+                          )
+                        ];
+                      } else {
+                        children = <Widget>[
+                          ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: jsonSnap["users"].length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage: NetworkImage("https://picsum.photos/seed/picsum/200"),
+                                ),
+                                title: Text(jsonSnap["users"][index]["display_name"]),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ChatPage(name: jsonSnap["users"][index]["display_name"], id: jsonSnap["users"][index]["user_id"], display: displayName, sender: userId, direct: true)
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                          ),
+                        ];
+                      }
+                    }
+                    else if (snapshot.hasError) {
+                      children = <Widget>[
+                        new Container(
+                          color: Colors.white,
+                          child: Center(
+                            child: Text(
+                              "There was an error fetching your channels, please check your internet connection and try again in a second."
+                            )
+                          )
+                        )
+                      ];
+                    }
+                  } else {
+                    return new Container(
+                      color: Colors.white,
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.white,
+                          valueColor: new AlwaysStoppedAnimation<Color>(Colors.red[200])
+                        )
+                      )
+                    );
+                  }
+                  return Container(
+                    color: Colors.white,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: children,
+                      )
+                    ),
+                  );
+                }
             )
-          ];
-        },
-        body: TabBarView(
-          controller: _tabController,
-          children: <Widget> [
-            FutureBuilder<String>(
-              future: getChannels(),
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                List<Widget> children;
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasData && snapshot.data != null) {
-                    var jsonSnap = json.decode(snapshot.data);
-                    if (jsonSnap["channels"].length == 0) {
-                      children = <Widget>[
-                        new Container(
-                          color: Colors.white,
-                          child: Center(
-                            child: Text(
-                              "You haven't joined any channels yet! Head over to the discover page to join some!"
-                            )
-                          )
-                        )
-                      ];
-                    } else {
-                      children = <Widget>[
-                        ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: jsonSnap["channels"].length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage("https://picsum.photos/seed/picsum/200"),
-                              ),
-                              title: Text(jsonSnap["channels"][index]["name"]),
-                              subtitle: Text(jsonSnap["channels"][index]["description"]),
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => ChatPage(name: jsonSnap["channels"][index]["name"], id: jsonSnap["channels"][index]["channel_id"], display: displayName, sender: userId, direct: false)
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ];
-                    }
-                  } else if (snapshot.hasError) {
-                    children = <Widget>[
-                      new Container(
-                        color: Colors.white,
-                        child: Center(
-                          child: Text(
-                            "There was an error fetching your channels, please check your internet connection and try again in a second."
-                          )
-                        )
-                      )
-                    ];
-                  }
-                } else {
-                  return new Container(
-                    color: Colors.white,
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.white,
-                        valueColor: new AlwaysStoppedAnimation<Color>(Colors.red[200])
-                      )
-                    )
-                  );
-                }
-                return Container(
-                  color: Colors.white,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: children,
-                    )
-                  ),
-                );
-              }
-            ),
-            FutureBuilder<String>(
-              future: getDirect(),
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                List<Widget> children;
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasData && snapshot.data != null) {
-                    var jsonSnap = json.decode(snapshot.data);
-                    if (jsonSnap["users"].length == 0) {
-                      children = <Widget>[
-                        new Container(
-                          color: Colors.white,
-                          child: Center(
-                            child: Text(
-                              "You haven't sent any direct messages yet! Use the button above to send some!"
-                            )
-                          )
-                        )
-                      ];
-                    } else {
-                      children = <Widget>[
-                        ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          physics: const NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: jsonSnap["users"].length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage("https://picsum.photos/seed/picsum/200"),
-                              ),
-                              title: Text(jsonSnap["users"][index]["display_name"]),
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => ChatPage(name: jsonSnap["users"][index]["display_name"], id: jsonSnap["users"][index]["user_id"], display: displayName, sender: userId, direct: true)
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                        ),
-                      ];
-                    }
-                  }
-                  else if (snapshot.hasError) {
-                    children = <Widget>[
-                      new Container(
-                        color: Colors.white,
-                        child: Center(
-                          child: Text(
-                            "There was an error fetching your channels, please check your internet connection and try again in a second."
-                          )
-                        )
-                      )
-                    ];
-                  }
-                } else {
-                  return new Container(
-                    color: Colors.white,
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.white,
-                        valueColor: new AlwaysStoppedAnimation<Color>(Colors.red[200])
-                      )
-                    )
-                  );
-                }
-                return Container(
-                  color: Colors.white,
-                  child: SingleChildScrollView(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: children,
-                    )
-                  ),
-                );
-              }
-          )
-        ]
+          ]
+        )
+      ),
       )
-    ),
     );
   }
 }
