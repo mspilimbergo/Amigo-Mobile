470ed9421e19365f29616d12a403b8faddfded51
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-05 03:22:28 -0400

    Added initial screen for the profile page

---
+++ lib/screens/profile/profile_edit_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
