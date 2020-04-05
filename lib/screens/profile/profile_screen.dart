cc6bedc2e8f1fd1b50277d40b26f0f7aa3b58186
dylans123 <dylanskelly@knights.ucf.edu>
2020-04-05 02:27:44 -0400

    Added mail field

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -130,7 +130,32 @@ class ProfilePage extends StatelessWidget {
                         )
                       )
                     ],
-                  )
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(right: 20.0),
+                        child: Icon(
+                          Icons.mail_outline,
+                          color: Colors.black54,
+                          size: 50.0,
+                        )
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 20.0),
+                        child: 
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("Email", style: TextStyle(fontSize: 12.0, color: Colors.black54)),
+                            Text("Email", style: TextStyle(fontSize: 18.0, color: Colors.black))
+                          ],
+                        )
+                      )
+                    ],
+                  ),
                 ],
               ),
             ),
